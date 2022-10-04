50caa7c8e1081a5c98dae9817cf597b3f8cc35e1
Aakarsh Agarwal <aakarsh.agarwal@Aakarshs-MacBook-Air.local>
2022-10-04 00:24:57 -0500

    Final version

--- src/main/java/com/example/fetchrewards/model/AddTxnRequest.java
+++ src/main/java/com/example/fetchrewards/model/AddTxnRequest.java
@@ -2,6 +2,7 @@ package com.example.fetchrewards.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class AddTxnRequest {
     private String payer;
